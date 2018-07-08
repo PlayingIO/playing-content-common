d29ca28b3cb0c000d084b4ee2c9495af13cd0f6f
forafish <fora.luo@gmail.com>
2018-07-09 05:01:43 +0800

    Fix moveDocument with predefined path

--- src/helpers/move-document.js
+++ src/helpers/move-document.js
@@ -2,12 +2,12 @@ import path from 'path';
 import { plural } from 'pluralize';
 
 /**
- * Move documents to target
+ * Move documents
  */
-export default async function moveDocument (app, doc, target) {
+export default async function moveDocument (app, doc) {
   const svcService = app.service(plural(doc.type || 'document'));
   const data = {
-    path: path.resolve(target, path.basename(doc.path)),
+    path: doc.path, // recompute path
     type: doc.type
   };
   return svcService.patch(doc.id, data);
