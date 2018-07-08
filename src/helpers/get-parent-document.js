909e0c97524ceed701ae002f083ffd9f280b320d
forafish <fora.luo@gmail.com>
2018-07-08 23:11:23 +0800

    rename param doc to data

--- src/helpers/get-parent-document.js
+++ src/helpers/get-parent-document.js
@@ -4,23 +4,24 @@ import { plural } from 'pluralize';
 import isRootFolder from './is-root-folder';
 
 // get parent or root or top workspaces document
-export default async function getParentDocument (app, id, doc) {
+export default async function getParentDocument (app, id, data) {
   // get by the parent if provided
-  if (doc.parent) {
+  if (data.parent) {
     const svcDocuments = app.service('documents');
-    return svcDocuments.get(doc.parent);
+    return svcDocuments.get(data.parent);
   }
   // get by id and type
   if (id) {
-    const svcDocuments = app.service(plural(doc && doc.type || 'document'));
-    return svcDocuments.get(id, {
+    const svcDocuments = app.service(plural(data && data.type || 'document'));
+    const doc = await svcDocuments.get(id, {
       query: { $select: 'parent,*' }
-    }).then(doc => doc && doc.parent);
+    });
+    return doc && doc.parent;
   }
   // get the root folder or workspaces root
-  if (!isRootFolder(doc.path)) {
+  if (!isRootFolder(data.path)) {
     const svcFolder = app.service('folders');
-    if (doc.path.startsWith('/workspaces')) {
+    if (data.path.startsWith('/workspaces')) {
       return svcFolder.get(null, { query: { path : '/workspaces' } });
     } else {
       return svcFolder.get(null, { query: { path : '/' } });
