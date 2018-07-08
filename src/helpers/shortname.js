1f08246a94430f99c35ae41fa39241b3bb50d46a
forafish <fora.luo@gmail.com>
2018-07-08 23:35:52 +0800

    Comment shortname

--- src/helpers/shortname.js
+++ src/helpers/shortname.js
@@ -3,13 +3,16 @@ import path from 'path';
 import shortid from 'shortid';
 import slug from 'limax';
 
-// generate a short typed name for a document
+/**
+ * Generate a short typed name for a document, using exisiting name
+ * or slug by provided title
+ */
 export default function shortname (type, existing, title) {
   let name = type + '-' + shortid.generate();
   if (existing) {
     name = path.basename(existing); // existing short name
   } else if (title) {
-    name = type + '-' + slug(title, { tone: false });
+    name = type + '-' + slug(title, { tone: false }); // slug by title
   }
   // if name does not contain and start with doc type, add doc type to name
   if (name.indexOf('-') < 0 && !name.startsWith(type)) {
