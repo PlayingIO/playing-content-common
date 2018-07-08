466085a6a0d96bfdf5f73815e5f12c8fa73d0a0b
forafish <fora.luo@gmail.com>
2018-07-09 05:04:04 +0800

    Fix fanoutDocuments with predefined path

--- src/helpers/fanout-documents.js
+++ src/helpers/fanout-documents.js
@@ -1,10 +1,14 @@
 import fp from 'mostly-func';
+import path from 'path';
+
+import shortname from './shortname';
 
 /**
  * Get children documents
  */
-const getChildrens = async (app, parents) => {
+const getChildrens = async (app, documents) => {
   const svcDocuments = app.service('documents');
+  const parents = fp.map(fp.prop('id'), documents);
   const children = await svcDocuments.find({
     query: { parent: { $in: parents } },
     paginate: false
@@ -15,17 +19,22 @@ const getChildrens = async (app, parents) => {
 /**
  * Fanout move/copy operation to children documents
  */
-export default async function fanoutDocuments (app, documents, operation, target) {
-  // get children documents with skip/limit
-  const parents = fp.map(fp.prop('id'), documents);
-  const childrens = await getChildrens(app, parents);
-  if (!fp.isEmpty(childrens)) {
-    for (const [parent, children] of Object.entries(childrens)) {
+export default async function fanoutDocuments (app, documents, operation, targets) {
+  const childrens = await getChildrens(app, documents);
+  for (let [parent, children] of Object.entries(childrens)) {
+    const target = targets[parent];
+    if (children.length > 0 && target) {
+      // new path
+      children = fp.map(doc => fp.assoc('path', path.join(target, shortname(doc.type)), doc), children);
       app.agenda.now('fanout_documents', {
-        operation, documents: children, target: target
+        operation, documents: children
       });
       // process next batch of children
-      await fanoutDocuments(app, children, operation, parent);
+      const childrenTargets = fp.reduce((acc, child) => {
+        acc[child.id] = child.path;
+        return acc;
+      }, {}, children);
+      await fanoutDocuments(app, children, operation, childrenTargets);
     }
   }
 }
